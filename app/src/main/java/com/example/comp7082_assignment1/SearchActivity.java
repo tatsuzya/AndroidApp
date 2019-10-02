f2019402ebf0a72869e2fc3e0face29b11867e0a
fredwongg <fwong91@my.bcit.ca>
2019-10-01 20:59:49 -0700

    fix activity_search.xml

--- app/src/main/java/com/example/comp7082_assignment1/SearchActivity.java
+++ app/src/main/java/com/example/comp7082_assignment1/SearchActivity.java
@@ -2,7 +2,10 @@ package com.example.comp7082_assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Button btnCancel = (Button)findViewById(R.id.button_cancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SearchActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
