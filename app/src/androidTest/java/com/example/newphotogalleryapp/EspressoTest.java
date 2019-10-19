9e292115d6039a19656754b2be694b63fbce233b
Shiv <shivbirr@gmail.com>
2019-10-18 19:19:56 -0700

    added another test, still need to do location tag test

--- app/src/androidTest/java/com/example/newphotogalleryapp/EspressoTest.java
+++ app/src/androidTest/java/com/example/newphotogalleryapp/EspressoTest.java
@@ -1,5 +1,6 @@
 package com.example.newphotogalleryapp;
 
+import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -10,9 +11,12 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.StringContains.containsString;
+import static androidx.test.espresso.intent.Intents.intending;
 
 public class EspressoTest {
     @Rule
@@ -20,7 +24,7 @@ public class EspressoTest {
 
     @Test
     //tests if date search works
-    public void Test1() {
+    public void TestTag() {
         onView(withId(R.id.button_search)).perform(click());
         onView(withId(R.id.startDate)).perform(typeText("20191015"));
         onView(withId(R.id.endDate)).perform(typeText("20191015"));
@@ -28,6 +32,13 @@ public class EspressoTest {
         onView(withId(R.id.button_search)).perform(click());
         // onView(withId(R.id.timestamp_textview)).check(matches(withText("20191015")));
         onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString("20191015"))));
+    }
+
+    @Test
+    public void TestSearchIntentOpens(){
+        Intents.init();
+        onView(withId(R.id.button_search)).perform(click());
+        intended(hasComponent(SearchActivity.class.getName()));
 
     }
 }
