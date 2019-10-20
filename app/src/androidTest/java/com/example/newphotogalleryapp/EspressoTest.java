89f0dd069d875c5d384175f2452ab9389a4c4cf9
Shiv <shivbirr@gmail.com>
2019-10-20 16:14:02 -0700

    testbug fix

--- app/src/androidTest/java/com/example/newphotogalleryapp/EspressoTest.java
+++ app/src/androidTest/java/com/example/newphotogalleryapp/EspressoTest.java
@@ -13,8 +13,10 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.StringContains.containsString;
 import static androidx.test.espresso.intent.Intents.intending;
 
@@ -25,20 +27,90 @@ public class EspressoTest {
     @Test
     //tests if date search works
     public void TestTag() {
+        String d1 = "20191020";
         onView(withId(R.id.button_search)).perform(click());
-        onView(withId(R.id.startDate)).perform(typeText("20191015"));
-        onView(withId(R.id.endDate)).perform(typeText("20191015"));
+        onView(withId(R.id.startDate)).perform(typeText(d1));
+        onView(withId(R.id.endDate)).perform(typeText(d1));
         closeSoftKeyboard();
         onView(withId(R.id.button_search)).perform(click());
-        // onView(withId(R.id.timestamp_textview)).check(matches(withText("20191015")));
-        onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString("20191015"))));
+        onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString(d1))));
+        for (int i = 1; i < 9; i++) {
+            onView(withId(R.id.button_right)).perform(click());
+        }
     }
 
     @Test
-    public void TestSearchIntentOpens(){
+    public void TestSearchIntentOpens() {
         Intents.init();
         onView(withId(R.id.button_search)).perform(click());
         intended(hasComponent(SearchActivity.class.getName()));
+
+    }
+
+    //testing use of single word tag
+    @Test
+    public void WordTag() {
+        String w1 = "health";
+
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.Tags)).perform(typeText(w1));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString(w1))));
+
+    }
+
+    //testing use of 2 word tags
+    @Test
+    public void WordTag2() {
+        String w2 = "math";
+
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.Tags)).perform(typeText(w2));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString(w2))));
+
+    }
+    //testing use of 2 word tags
+    @Test
+    public void bigtagtest() {
+        String w5 = "VERYVERYVERYVERYVERYVERYBIGTAGTEST1111111111111111111";
+
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.Tags)).perform(typeText(w5));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString(w5))));
+
+    }
+    @Test
+    public void WordTagMultiple() {
+        String w3 = "all";
+
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.Tags)).perform(typeText(w3));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_search)).perform(click());
+
+        for (int i = 1; i < 4; i++) {
+            onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString(w3))));
+            onView(withId(R.id.button_right)).perform(click());
+        }
+
+        onView(withId(R.id.timestamp_textview)).check(matches(withText(containsString(w3))));
+
+    }
+
+    @Test
+    public void NotWordTag() {
+        String w4 = "notInTags";
+
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.Tags)).perform(typeText(w4));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_search)).perform(click());
+        onView(withId(R.id.timestamp_textview)).check(matches(not(withText(containsString(w4)))));
 
     }
 }
